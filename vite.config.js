c53917afd9e6826a8c7d43343bc5a0bbbdbdd3bd
XianA-java <2873030027@qq.com>
2025-01-09 20:45:36 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  devServer:{
-    host: '0.0.0.0',
-    port: 5173
-  },
   plugins: [vue()],
   resolve: {
     alias: {
